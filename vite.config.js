a97f2faad7fe0723521ac188114f8b18c6910819
Adrian-751 <rodriguezadrian636@gmail.com>
2025-07-23 16:03:38 -0300

    Configuración para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Porfolio/', // <- Pon el nombre de tu repo aquí, incluyendo las barras
   plugins: [react()],
 })
