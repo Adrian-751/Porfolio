f1e8297d67a308b2202e22a035bdaa403bbb14fb
Adrian-751 <rodriguezadrian636@gmail.com>
2025-07-23 16:06:56 -0300

    Build para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Porfolio/', // <- Pon el nombre de tu repo aquí, incluyendo las barras
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react()],
 })
