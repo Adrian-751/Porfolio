43cea177d1e26b253d461c0ecca8cde682fa3d6a
Adrian-751 <rodriguezadrian636@gmail.com>
2025-07-24 15:03:50 -0300

    Fix ruta base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Porfolio/', // usa el nombre del repo o carpeta base si no es /
-});
+  base: '/Porfolio/',
+})
